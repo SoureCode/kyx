86d68b8f9a8a6771dba8d34314500fc1579b9826
chapterjason <jason.schilling@hotmail.de>
2025-05-27 22:47:36 +0200

    Add dependency check for Symfony framework in server start/stop commands

--- macro/symfony_server_start.go
+++ macro/symfony_server_start.go
@@ -1,20 +1,24 @@
 package macro
 
 import (
+	"github.com/SoureCode/kyx/project"
 	"github.com/SoureCode/kyx/shell"
 	"github.com/pkg/errors"
 )
 
 func SymfonyServerStart() {
 	logger := shell.GetLogger()
+	p := project.GetProject()
 
-	cmd, err := shell.NewSymfonyCommand("serve", "--daemon")
+	if p.HasDependency("symfony/framework-bundle") {
+		cmd, err := shell.NewSymfonyCommand("serve", "--daemon")
 
-	if err != nil {
-		panic(errors.Wrap(err, "failed to create Symfony command to start server"))
-	}
+		if err != nil {
+			panic(errors.Wrap(err, "failed to create Symfony command to start server"))
+		}
 
-	if err = cmd.WithLogger(logger).Run(); err != nil {
-		panic(errors.Wrap(err, "failed to execute command to start server"))
+		if err = cmd.WithLogger(logger).Run(); err != nil {
+			panic(errors.Wrap(err, "failed to execute command to start server"))
+		}
 	}
 }
--- macro/symfony_server_stop.go
+++ macro/symfony_server_stop.go
@@ -1,20 +1,24 @@
 package macro
 
 import (
+	"github.com/SoureCode/kyx/project"
 	"github.com/SoureCode/kyx/shell"
 	"github.com/pkg/errors"
 )
 
 func SymfonyServerStop() {
 	logger := shell.GetLogger()
+	p := project.GetProject()
 
-	cmd, err := shell.NewSymfonyCommand("server:stop")
+	if p.HasDependency("symfony/framework-bundle") {
+		cmd, err := shell.NewSymfonyCommand("server:stop")
 
-	if err != nil {
-		panic(errors.Wrap(err, "failed to create Symfony command to stop server"))
-	}
+		if err != nil {
+			panic(errors.Wrap(err, "failed to create Symfony command to stop server"))
+		}
 
-	if err = cmd.WithLogger(logger).Run(); err != nil {
-		panic(errors.Wrap(err, "failed to execute command to stop server"))
+		if err = cmd.WithLogger(logger).Run(); err != nil {
+			panic(errors.Wrap(err, "failed to execute command to stop server"))
+		}
 	}
 }
